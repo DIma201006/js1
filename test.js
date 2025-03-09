21cf55a31d3812ca0d1898a44235c7e7b6eac3fc
DIma201006 <166295049+DIma201006@users.noreply.github.com>
2025-03-09 17:38:51 +0300

    fff

---
+++ test.js
@@ -0,0 +1,30 @@
+const age = 14;
+console.log(age);
+
+const name = "Dima";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Прагніть не до успіху, а до цінностей, які він дає";
+console.log(myString);
+
+let myNumber = 5 + 7;
+ myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім’я");
+const tablesName = alert(userName);
+const typeOfChange = (typeof (userName));
+console.log(typeOfChange);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+const danger = alert("Увага! Дія може бути небезпечною.")
+const сonsent = confirm("Ви впевнені, що хочете продовжити?")
+console.log(danger)
