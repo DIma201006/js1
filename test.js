c3ca20683023047deed79983ac71d77b2651b43b
DIma201006 <166295049+DIma201006@users.noreply.github.com>
2025-03-22 16:44:18 +0300

    js

--- test.js
+++ test.js
@@ -37,54 +37,78 @@
 
 // Home-Work 2
 
-const celsius = 0;
-let fahrenheit;
-fahrenheit = 9 / 5 * celsius + 32;
-console.log(fahrenheit);
+// const celsius = 0;
+// let fahrenheit;
+// fahrenheit = 9 / 5 * celsius + 32;
+// console.log(fahrenheit);
 
-const daysInMonth = 31;
-const hoursInMonth = 24 * daysInMonth;
-const minutesInMonth = 60 * hoursInMonth;
-console.log(hoursInMonth + " годин,", minutesInMonth + " хвилин");
+// const daysInMonth = 31;
+// const hoursInMonth = 24 * daysInMonth;
+// const minutesInMonth = 60 * hoursInMonth;
+// console.log(hoursInMonth + " годин,", minutesInMonth + " хвилин");
 
 
  
-let health = 50;
-const damage = 10;
-let energy = 80;
-const step = 20;
-health = health - damage;
-energy = energy - step;
-console.log(health + " health,", energy + " energy");
+// let health = 50;
+// const damage = 10;
+// let energy = 80;
+// const step = 20;
+// health = health - damage;
+// energy = energy - step;
+// console.log(health + " health,", energy + " energy");
 
 
-const totalPrice = 70;
-const discount = 0.1;
-const discountedPrice = totalPrice - discount * totalPrice;
-console.log(discountedPrice);
+// const totalPrice = 70;
+// const discount = 0.1;
+// const discountedPrice = totalPrice - discount * totalPrice;
+// console.log(discountedPrice);
 
 
-const floatNumber = 12.78;
-const roundedDown = Math.floor(floatNumber);
-console.log(roundedDown);
+// const floatNumber = 12.78;
+// const roundedDown = Math.floor(floatNumber);
+// console.log(roundedDown);
 
-const floatString = "45.67";
-const parsedFloat = parseFloat(floatString);
-console.log(parsedFloat);
+// const floatString = "45.67";
+// const parsedFloat = parseFloat(floatString);
+// console.log(parsedFloat);
 
-const intString = "123";
-const parsedInt = parseInt(intString);
-console.log(parsedInt);
+// const intString = "123";
+// const parsedInt = parseInt(intString);
+// console.log(parsedInt);
 
-const number = 16;
-const sqrtNumber = Math.sqrt(number);
-console.log(sqrtNumber);
+// const number = 16;
+// const sqrtNumber = Math.sqrt(number);
+// console.log(sqrtNumber);
 
-const integer = 42;
-const stringNumber = "256";
-const convertedInt = parseInt(stringNumber);
-console.log(convertedInt);
-const convertedString = integer.toString();
-console.log(convertedString);
+// const integer = 42;
+// const stringNumber = "256";
+// const convertedInt = parseInt(stringNumber);
+// console.log(convertedInt);
+// const convertedString = integer.toString();
+// console.log(convertedString);
 
+
+
+// Home-Work 3
+
+const email = "dima213123@gmail.com";
+const result = email.includes("@");
+const symbols = email.length;
+console.log(result);
+console.log(Symbols);
+
+const resultat = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+const word1 = "my ";
+const word2 = "name ";
+const word3 = "is ";
+let fullName = word1 + word2 + word3;
+fullName += "Viktor";
+console.log(fullName);
+
+const userName = "Олександро";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
 
