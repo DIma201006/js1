ec5fdf9876e8f3d07aabbae740adf0345b157b80
DIma201006 <166295049+DIma201006@users.noreply.github.com>
2025-04-09 19:59:43 +0300

    fix

--- test2.js
+++ test2.js
@@ -1,1 +1,48 @@
+// Home-Work 4
 
+
+const email = "@DIMA";
+const name = "Nazar";
+if (email != null && name) {
+ console.log("Обидва поля заповнені");
+} else {
+    console.log("Не всі поля заповнені");
+}
+
+
+const str1 = 5;
+const str2 = 5;
+if (str1 + str2 < 10) {
+    console.log("Сума менша або дорівнює 10");
+}else {console.log("Сума більша або дорівнює 10");
+}
+
+
+ const str = "JavaScript";
+ if (str != "JavaScript") {
+     console.log("Текст не містить  слово JavaScript");
+ }else {console.log("Текст містить слово JavaScript");
+ }
+
+
+
+const number = 10
+if (number > 10 && number <20) {
+    console.log("Число входить в діапазон від 10 до 20");
+}else {console.log("Число не входить в діапазон від 10 до 20");
+}
+
+
+
+
+
+const name1 = "Ivan"
+const email1 = "@ggggg."
+const password = "123456"
+if (
+    name1.length >= 3 &&
+    email1.includes("@") && email1.indexOf(".") &&
+    password.length >= 6
+) {console.log("Перенаправлення на іншу сторінку");
+}else {console.log("Помилка: неправильне заповнення");
+}
